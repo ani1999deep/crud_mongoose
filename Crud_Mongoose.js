3dcd6849829ed98e971140f3daccd90d023995c6
ani1999deep <anideepbhowmick1@gmail.com>
2023-05-03 14:19:27 +0530

    first commit

--- Crud_Mongoose.js
+++ Crud_Mongoose.js
@@ -1,25 +1,51 @@
 const mongoose = require('mongoose');
-const main = async () => {
-    await mongoose.connect("mongodb://0.0.0.0:27017/e-comm");
-    //Schema
-    const ProductSchema = new mongoose.Schema({
 
-        name: String,
-        price:Number,
-        brand:String,
-        category:String
+mongoose.connect('mongodb://0.0.0.0:27017/e-comm');
+const productSchema = new mongoose.Schema({
+    name: String,
+    price: Number,
+    brand: String,
+    category: String
 
+});
+
+const saveInDB = async () => {
+    const Product = mongoose.model('products', productSchema);
+    let data = new Product({
+        name: "max 100",
+        price: 200,
+        brand: 'maxx',
+        category: 'Mobile'
     });
-
-    const ProductModel = mongoose.model('products', ProductSchema);
-
-    let data = new ProductModel({ 
-    name: "Nde pro",
-    price:500,
-    brand:"text",
-    category:"Mobile"
-})
-    let result = await data.save();
-    console.log(result)
+    const result = await data.save();
+    console.log(result);
 }
-main()
+
+//Update the field
+
+const updateInDB =async  () => {
+    const Product = mongoose.model('products', productSchema);
+    let data =await  Product.updateOne(
+        { name: "max 6" },
+        {
+            $set: { price: 550,name:'max pro 6' }
+        }
+    )
+    console.log(data)
+}
+//Delete the field record
+
+const deleteInDB = async ()=>{
+    const Product = mongoose.model('products', productSchema);
+    let data = await Product.deleteOne({name:'max 100'})
+    console.log(data);
+}
+
+
+const findInDB = async ()=>{
+    const Product = mongoose.model('products', productSchema);
+    let data = await Product.find({name:'max 500'})
+    console.log(data);
+}
+
+findInDB();
